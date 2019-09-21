081f04562fb644e7a7d7b1143a6d380f3531849b
joaochioda <joao.chioda@dextra-sw.com>
2019-09-21 18:28:19 -0300

    m

--- src/main/java/com/example/heroku2/model/Person.java
+++ src/main/java/com/example/heroku2/model/Person.java
@@ -1,76 +1,14 @@
 package com.example.heroku2.model;
 
-import javax.persistence.*;
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
-@TableGenerator(name="tab", initialValue=0, allocationSize=50)
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE, generator = "tab")
     private Long id;
     private String name;
-    private String sobrenome;
-    private String email;
-    private String rg;
-    private Double pontos;
     private String phone;
-//    @ManyToMany
-//    private List<Essencia> essencia;
-
-    public Person() {
-
-    }
-
-    public Person(String name, String sobrenome, String email, String rg, Double pontos, String phone, List<Essencia> essencia) {
-        this.name = name;
-        this.sobrenome = sobrenome;
-        this.email = email;
-        this.rg = rg;
-        this.pontos = pontos;
-        this.phone = phone;
-//        this.essencia = essencia;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRg() {
-        return rg;
-    }
-
-    public void setRg(String rg) {
-        this.rg = rg;
-    }
-
-    public Double getPontos() {
-        return pontos;
-    }
-
-    public void setPontos(Double pontos) {
-        this.pontos = pontos;
-    }
-
-//    public List<Essencia> getEssencia() {
-//        return essencia;
-//    }
-
-//    public void setEssencia(List<Essencia> essencia) {
-//        this.essencia = essencia;
-//    }
 
     public String getPhone() {
         return phone;
