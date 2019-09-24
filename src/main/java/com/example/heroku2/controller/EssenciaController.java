ec65c90c0e001ba094535a4634b4dd9abbaf4969
joaochioda <joao.chioda@dextra-sw.com>
2019-09-24 18:35:29 -0300

    get essencia

--- src/main/java/com/example/heroku2/controller/EssenciaController.java
+++ src/main/java/com/example/heroku2/controller/EssenciaController.java
@@ -5,6 +5,8 @@ import com.example.heroku2.service.EssenciaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 public class EssenciaController {
 
@@ -14,5 +16,15 @@ public class EssenciaController {
     @RequestMapping(method = RequestMethod.POST, value= "/essencia")
     public Essencia addEssencia(@RequestBody Essencia essencia) {
         return essenciaService.addEssencia(essencia);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value="/essencia/{id}")
+    public Essencia getEssenciaById(@PathVariable Long id) {
+        return essenciaService.getEssenciaById(id);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/essencia")
+    public List<Essencia> getAllEssencia() {
+        return essenciaService.getAllEssencia();
     }
 }
--- src/main/java/com/example/heroku2/service/EssenciaService.java
+++ src/main/java/com/example/heroku2/service/EssenciaService.java
@@ -5,6 +5,10 @@ import com.example.heroku2.repository.EssenciaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EssenciaService {
 
@@ -13,5 +17,19 @@ public class EssenciaService {
 
     public Essencia addEssencia(Essencia essencia) {
         return essenciaRepository.save(essencia);
+    }
+
+    public Essencia getEssenciaById(Long id) {
+        Optional<Essencia> essencia = essenciaRepository.findById(id);
+        if(essencia.isPresent()){
+            return essencia.get();
+        }
+        return null;
+    }
+
+    public List<Essencia> getAllEssencia() {
+        List<Essencia> essencias = new ArrayList<>();
+        essenciaRepository.findAll().forEach(essencias::add);
+        return essencias;
     }
 }
